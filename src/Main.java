869ea343c655e34d72049a81b15a45b6307c19b7
Alexander Cedervall <astc@protonmail.com>
2024-12-05 13:45:12 +0100

    Review Change 1

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,33 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
-        if(args.length != 2) {
-            System.err.println("Missing required parameters: <pattern> <file>");
-            return;
+    public static void main(String[] args) throws IOException {
+        if(args.length != 3) {
+            System.err.println("Wrong number of parameters. Expected: search <pattern> <file>");
+            System.exit(1);
         }
 
-        Pattern pattern = Pattern.compile(args[0]);
+        if(!Objects.equals(args[0], "search")) {
+            System.err.println("Incorrect first parameter: " + args[0]);
+            System.exit(1);
+        }
 
+        findPattern(args[1], args[2]);
+    }
+
+    static void findPattern(String pattern, String file) throws IOException {
         try {
-            BufferedReader br = new BufferedReader(new FileReader(args[1]));
+            BufferedReader br = new BufferedReader(new FileReader(file));
             for(String line; (line = br.readLine()) != null;) {
-                if(pattern.matcher(line).find()) {
+                if(line.contains(pattern)) {
                     System.out.println(line);
                 }
             }
         } catch (FileNotFoundException e)  {
-            System.err.println("Could not find file: " + args[1]);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Could not find file: " + file);
         }
     }
 }
